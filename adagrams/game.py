687cc0b368c84d2bf28082384f99a9acf8312c33
Esther Kang <estherkang.j@gmail.com>
2025-03-14 13:38:43 -0700

    changed LETTER_POOL data organization

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,35 @@
 from random import randint
 
+# constants declaration
+LETTER_POOL = {
+    1: {'A': 9}, 
+    2: {'B': 2}, 
+    3: {'C': 2}, 
+    4: {'D': 4}, 
+    5: {'E': 12}, 
+    6: {'F': 2}, 
+    7: {'G': 3}, 
+    8: {'H': 2}, 
+    9: {'I': 9}, 
+    10: {'J': 1}, 
+    11: {'K': 1}, 
+    12: {'L': 4}, 
+    13: {'M': 2}, 
+    14: {'N': 6}, 
+    15: {'O': 8}, 
+    16: {'P': 2}, 
+    17: {'Q': 1}, 
+    18: {'R': 6}, 
+    19: {'S': 4}, 
+    20: {'T': 6}, 
+    21: {'U': 4}, 
+    22: {'V': 2}, 
+    23: {'W': 2}, 
+    24: {'X': 1}, 
+    25: {'Y': 2}, 
+    26: {'Z': 1}
+}
+
 # function takes no parameters and returns array of ten strings representing 10 tiles of letters
 def draw_letters():
     pass
